79cc9ef2252b45a33395f6ee77cbc118bc92adc8
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-10-14 17:09:16 -0300

    style: success message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const inputText = document.querySelector("#email");
 const button = document.querySelector("#button");
-console.log(inputText);
 const pMessage = document.querySelector(".error-message");
+console.log(pMessage);
 
 const validEmail = (email) => {
   const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
@@ -12,7 +12,8 @@ const buttonClick = () => {
   const email = inputText.value;
   const valid = validEmail(email);
   if (valid) {
-    pMessage.textContent = "";
+    pMessage.textContent = "Thanks for subscribing!";
+    pMessage.style.color = "#90EE90"
     inputText.style.borderColor = "";
   } else {
     inputText.style.backgroundColor = "rgba(255,0,0,0.2)";
