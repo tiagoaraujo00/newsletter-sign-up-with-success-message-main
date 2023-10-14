bf1f10e1b7d56fd37b49ca5969ee56c6840b8573
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-10-14 11:07:37 -0300

    feat: valid email required

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const inputText = document.querySelector("#email");
+const button = document.querySelector("#button");
+console.log(inputText);
+const pMessage = document.querySelector(".error-message");
+
+const validEmail = (email) => {
+  const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+  return regex.test(email);
+};
+
+const buttonClick = () => {
+  const email = inputText.value;
+  const valid = validEmail(email);
+  if (valid) {
+    pMessage.textContent = "";
+  } else {
+    inputText.style.backgroundColor = "rgba(255,0,0,0.5)";
+    inputText.style.borderColor = "red";
+    pMessage.textContent = "Valid email required";
+  }
+};
+
+button.addEventListener("click", buttonClick);
